937c27b13f1c7b0c51bb7eb6d156d925e6eb5de2
Sara Szwejkowska <saraszw@op.pl>
2023-06-14 19:21:04 +0200

    menu updated

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,71 +1,73 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import styles from './Header.module.scss';
 import banner from '../img/banner.png';
 import waves from '../img/waves (2).svg';
 import { AiOutlineComment } from 'react-icons/ai';
 import { BsTelephoneForwardFill } from 'react-icons/bs';
-import { FcLike } from "react-icons/fc";
+import { FcLike } from 'react-icons/fc';
 const Header = () => {
+	const [open, setIsOpen] = useState(false);
 
-const[open, setIsOpen] = useState(false)
-
-const toggleMenu = ( ) => {
-	setIsOpen((open) => !open)
-}
+	const toggleMenu = () => {
+		setIsOpen((open) => !open);
+	};
 
 	return (
 		<>
-		<section className={styles.header}>
-			<div className={styles.menuContact}>
-				<li className={styles.headerLink}>
-				 <FcLike  onClick={() =>setIsOpen((prev) => !prev)}
-				 />
-				 (
-					<span className={styles.iconMessage}>
-						{' '}
-						<AiOutlineComment />{' '}
+			<section className={styles.header}>
+				<div className={styles.menuContact}>
+					<div className={styles.heart}>
+						<FcLike onClick={() => setIsOpen((prev) => !prev)} />
+					</div>
+
+					{!open && (
+						<>
+							{' '}
+							<span className={styles.iconMessage}>
+								{' '}
+								<AiOutlineComment />{' '}
+							</span>
+							info@thecreatives360
+							<span className={styles.headerLinkTelephone}>
+								<li className={styles.iconTelephone}>
+									<BsTelephoneForwardFill />{' '}
+								</li>
+								+971 50 267 1236
+							</span>
+							<li className={styles.headerLink}>Services</li>
+							<li className={styles.headerLink}>Blog</li>
+							<li className={styles.headerLink}>Contact us</li>{' '}
+						</>
+					)}
+				</div>
+
+				<div className={styles.wrapperImg}>
+					<img
+						src={banner}
+						className={styles.bannerImg}
+						alt='people with pen'
+					/>
+				</div>
+				<div className={styles.headerText}>
+					<span className={styles.mainText}>
+						EFFECTIVE DIGITAL CAMPAIGNS THAT DRIVE MORE SALES{' '}
 					</span>
-					info@thecreatives360
-				</li>
+					<p className={styles.smallerTextHeader}>
+						WEBSITE DEVELOPMENT | APP DEVELOPMENT | BRANDING | SOCIAL MEDIA
+						MARKETING | SEO | GOOGLE ADs | AMAZON & NOON PRODUCT LISTING
+					</p>
+					<button className={styles.consultationBtn}>
+						<h4> Request a free consultation </h4>
+					</button>
+				</div>
 
-				<span className={styles.headerLinkTelephone}>
-					<li className={styles.iconTelephone}>
-						<BsTelephoneForwardFill />{' '}
-					</li>
-					+971 50 267 1236
-				</span>
+				<div className={styles.dot}></div>
+				<div className={styles.square}></div>
 
-				<li className={styles.headerLink}>Services</li>
-				<li className={styles.headerLink}>Blog</li>
-				<li className={styles.headerLink}>Contact us</li>	
-				 )
-			</div>
-			
-					
-
-			<div className={styles.wrapperImg}>
-				<img src={banner} className={styles.bannerImg} alt='people with pen' />
-			</div>
-			<div className={styles.headerText}>
-				<span className={styles.mainText}>
-					EFFECTIVE DIGITAL CAMPAIGNS THAT DRIVE MORE SALES{' '}
-				</span>
-				<p className={styles.smallerTextHeader}>
-					WEBSITE DEVELOPMENT | APP DEVELOPMENT | BRANDING | SOCIAL MEDIA
-					MARKETING | SEO | GOOGLE ADs | AMAZON & NOON PRODUCT LISTING
-				</p>
-				<button className={styles.consultationBtn}>
-					<h4> Request a free consultation </h4>
-				</button>
-			</div>
-
-			<div className={styles.dot}></div>
-			<div className={styles.square}></div>
-
-			<img src={waves} className={styles.wave} alt='' />
-		</section></>
-		
+				<img src={waves} className={styles.wave} alt='' />
+			</section>
+		</>
 	);
-}
-	
+};
+
 export default Header;
